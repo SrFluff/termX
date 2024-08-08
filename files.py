c6cbe0f451aa43cb49dd433809e92145e0738868
Franco <168871500+SrFluff@users.noreply.github.com>
2024-08-08 11:08:42 -0400

    Create files.py

---
+++ files.py
@@ -0,0 +1,15 @@
+aug = ['8', '9']
+
+eight = """
+First log, you should get
+some sleep buddy. You can
+upload and finish :)
+"""
+
+nine = """
+Second log, it is now on
+github and uses an external
+file for storing files.
+Use git clone https://github
+.com/SrFluff/termX
+"""
